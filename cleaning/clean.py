5cb57ca98476937685d159f4fed833e1fe647b25
LyndonFan <lyndon0808@gmail.com>
2021-10-05 00:24:40 +0100

    update cleaning

--- cleaning/clean.py
+++ cleaning/clean.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Set
 import pandas as pd
 import json
 import os
@@ -30,12 +30,14 @@ def col_rename(col: str) -> str:
 
 
 def clean_coalition_name(x: str) -> str:
+    x = x.replace("B.E.", "BE")
+    x = x.replace("R.I.R.", "RIR")
     x = x.replace(" - ", "-")
     x = re.sub("«|»", "", x)
     return x.upper()
 
 
-def min_idx(chars: str, s: str, start: int = 0) -> int:
+def min_idx(s: str, chars: str, start: int = 0) -> int:
     temp: str = s[start:]
     if not any(d in temp for d in chars):
         return -1
@@ -65,6 +67,7 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
 
     # clean party names and break down coalitions
     df.party = df.party.apply(clean_coalition_name)
+    unique_parties: Set[str] = set(df.party.unique())
 
     def coalition_to_parties(coalition: str) -> List[str]:
         if coalition[-1] == ".":
@@ -74,12 +77,19 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
         parties: List[str] = []
         idx: int = min_idx(coalition, ".-/")
         while idx >= 0 and idx < len(coalition) and any(d in coalition for d in ".-/"):
-            if coalition[:idx] in df.party:
+            if coalition[:idx] in unique_parties:
                 parties.append(coalition[:idx])
-                coalition = coalition[idx + 1 :]
+                coalition = coalition[idx + 1:]
+                idx = 0
+            elif coalition[:idx].replace(".", "") in unique_parties:
+                parties.append(coalition[:idx])
+                coalition = coalition[idx + 1:]
                 idx = 0
             idx = min_idx(coalition, ".-/", idx + 1)
-        return [coalition]
+        if coalition != "":
+            parties.append(coalition)
+        parties.sort()
+        return parties
 
     df["coalition"] = df.party
     coalition_lookup: Dict[str, List[str]] = {
@@ -98,7 +108,8 @@ if __name__ == "__main__":
     for y in range(2009, 2023, 4):
         dfs.append(
             pd.read_csv(
-                os.path.join(BASE_PATH, "final_csv", f"autarquicas_{y}_treated.csv"),
+                os.path.join(BASE_PATH, "final_csv",
+                             f"autarquicas_{y}_treated.csv"),
                 index_col=0,
             )
         )
